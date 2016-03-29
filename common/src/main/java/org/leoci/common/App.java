ae1cbeda5866ce2c398dff68e16a601f84c2b376
LEO <itaru1725@gmail.com>
2016-03-30 04:36:13 +0900

    update

---
+++ common/src/main/java/org/leoci/common/App.java
@@ -0,0 +1,13 @@
+package org.leoci.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
